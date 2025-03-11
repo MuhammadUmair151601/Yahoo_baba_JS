bd86ecf010fd51de92b04bb73324b77d46f7f63e
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-03-11 14:31:01 +0500

    ch02 done yahoo_baba

--- index.js
+++ index.js
@@ -6,4 +6,11 @@
 // its not loaded in website and give HTML in return we can say it is used in back end coding like databases php, nodejs, python etc
 // we will learn MERN stack mongo db, react, expressjs and nodejs.
 
-// *******************
+// *******************lecture 02***********************
+// applying JS in HTML file.
+// we can add js file in both the start of the head and the end of the body but in the end of the body it is more reliable as it resposnds fast as compared to the file linked before the  body
+// <head>
+// <script src = "index.js"> </script>----------------->for adding before body
+//    </head>
+// same as <body> ---------code --------------- <script src = "index.js"> </script> </body>
+// we can use the link from the external js file.
