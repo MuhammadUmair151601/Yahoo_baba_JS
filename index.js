80ad5da394c825599fd271b68657fb04c9a12235
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-03-11 14:26:32 +0500

    done with ch01 in js yahoo_baba

---
+++ index.js
@@ -0,0 +1,9 @@
+// ********************Lecture 01**********************
+// web base scripting language vs client side scripting language
+// ********web base scripting language*****************\
+// file is loaded from server and then run of web live.
+// ********Server side scripting language**************
+// its not loaded in website and give HTML in return we can say it is used in back end coding like databases php, nodejs, python etc
+// we will learn MERN stack mongo db, react, expressjs and nodejs.
+
+// *******************
